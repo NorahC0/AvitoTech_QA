0972b0d4965641333be709aa0e35348c55b97459
NorahC0 <61738690+NorahC0@users.noreply.github.com>
2025-02-16 18:50:47 +0300

    Add files via upload

--- test_ads.py
+++ test_ads.py
@@ -24,9 +24,11 @@ def test_create_ad():
 
 def test_get_ad_by_id():
     ad_id = "3301ca4d-7c21-41b1-aea2-40604ef246fc"
+    seller_id = 660974
     response = requests.get(f"{BASE_URL}/api/1/item/{ad_id}")
     assert response.status_code == 200
     assert response.json()[0]["id"] == ad_id
+    assert response.json()[0]["sellerId"] == seller_id
 
 
 def test_get_ads_by_seller_id():
@@ -41,6 +43,8 @@ def test_get_statistics_by_item_id():
     response = requests.get(f"{BASE_URL}/api/1/statistic/{ad_id}")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
+    assert "contacts" in response.json()[0]
+    assert "likes" in response.json()[0]
     assert "viewCount" in response.json()[0]
 
 
